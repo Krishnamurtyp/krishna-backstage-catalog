24e965b1fb6b31f32cf8809895846ed954a95165
krishnamurtyp <krishnamurthy.pradhan@gmail.com>
2025-02-14 16:49:41 +0530

    Initial commit - Backstage app setup

--- packages/app/src/components/catalog/EntityPage.tsx
+++ packages/app/src/components/catalog/EntityPage.tsx
@@ -163,6 +163,10 @@ const serviceEntityPage = (
       <EntityKubernetesContent />
     </EntityLayout.Route>
 
+    const serviceEntityPage = (
+
+);
+
     <EntityLayout.Route path="/api" title="API">
       <Grid container spacing={3} alignItems="stretch">
         <Grid item md={6}>
